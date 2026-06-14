1787f33feefdbcd54a29b6adf273df0adc0a908b
AlokaCode.Space <alokatekteach@gmail.com>
2026-06-14 09:05:52 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/eagle-eye-studio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
